ae4ee1aec6842c3aaebf5f3057c852e2d19f6800
goksy <gokhnder@gmail.com>
2024-05-31 14:33:20 +1000

    create main files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const grid = document.querySelector(".container");
+
+for(let i = 0; i< 16 ; i ++){
+    for (let j = 0; j < 16 ; j++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        //cell.textContent = `Cell ${i + 1}-${j + 1}`;
+        grid.appendChild(cell);
+        console.log(j);
+    }
+}
